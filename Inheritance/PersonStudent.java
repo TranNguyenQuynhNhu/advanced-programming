bd3199775d120109f1071be7546f8b1255770b1a
Tran Nguyen Quynh Nhu <trannguyenquynhnhu24@gmail.com>
2026-04-14 00:37:21 +0700

    Move file

---
+++ Inheritance/PersonStudent.java
@@ -0,0 +1,5 @@
+public class PersonStudent {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Inheritance/VehicleCar.java
@@ -0,0 +1,5 @@
+public class VehicleCar {
+    public static void main(String[] args) {
+        
+    }
+}
