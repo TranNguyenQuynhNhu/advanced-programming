6a19ca7a2c315f0f5283bc8b8872f1692a194e5e
Tran Nguyen Quynh Nhu <trannguyenquynhnhu24@gmail.com>
2026-04-14 19:32:29 +0700

    Person -> Student

--- Inheritance/PersonStudent.java
+++ Inheritance/PersonStudent.java
@@ -1,5 +1,55 @@
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println();
+    }
+}
+
+class Student extends Person {
+    private String studentId;
+
+    public Student(String name, int age, String studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Student ID: " + studentId);
+        System.out.println();
+    }
+}
+
 public class PersonStudent {
     public static void main(String[] args) {
-        
+        Person p = new Person("An", 30);
+        Student s = new Student("Nhu", 19, "S12345");
+        Person k = new Student("Hung", 19, "2452930");
+        p.displayInfo();
+        s.displayInfo();
+        k.displayInfo();
     }
 }
