484b08016c5a7d50eb943b8497027f2aa33e106d
Tran Nguyen Quynh Nhu <trannguyenquynhnhu24@gmail.com>
2026-04-14 21:19:47 +0700

    Composition

--- Inheritance/Composition.java
+++ Inheritance/Composition.java
@@ -1,5 +1,25 @@
+class Engine {
+    public void start() {
+        System.out.println("Engine started.");
+    }
+}
+
+class Car {
+    private Engine engine;
+
+    public Car() {
+        engine = new Engine();
+    }
+
+    public void startCar() {
+        System.out.println("Car is starting...");
+        engine.start();
+    }
+}
+
 public class Composition {
     public static void main(String[] args) {
-        
+        Car car = new Car();
+        car.startCar();
     }
 }
